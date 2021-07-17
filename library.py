7694df648274b36fa3f811e0202e0fdf5eef2091
中嶋栄光 <e.nakashima@gemcook.com>
2021-07-17 23:31:07 +0900

    test

--- library.py
+++ library.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# defaultdict
+
 s = "abcdefghisjsiskgfnodsgoan"
 
 d = {}
@@ -6,3 +9,19 @@ for c in s:
         d[c] = 0
     d[c] += 1
 print(d)
+
+d = {}
+for c in s:
+    d.setdefault(c, 0)
+    d[c] += 1
+print(d)
+
+# 標準ライブラリだが、importしないと使えない
+from collections import defaultdict
+
+d = defaultdict(int)
+
+for c in s:
+    d[c] += 1
+print(d)
+print(d["f"])
